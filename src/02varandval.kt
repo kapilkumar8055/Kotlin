0197d414f886c504666bca981a1fe47a82adb334
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-07-30 00:28:19 +0530

    var and val

---
+++ src/02varandval.kt
@@ -0,0 +1,23 @@
+fun main(){
+    //var-mutable
+    var wholeNumber=10
+    var fractionalNumber=0.5
+    var sentence="i am kapil kumar"
+    var condition=true;
+
+    //val-immutable
+    val number=12
+    val fraction=2.5
+    val sentence2="i am from mathura"
+    val condition2=true
+
+    println(wholeNumber)
+    println(fractionalNumber)
+    println(sentence)
+    println(condition)
+
+    println(number)
+    println(fraction)
+    println(sentence2)
+    println(condition2)
+}
