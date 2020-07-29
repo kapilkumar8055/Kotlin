36b67d43a20a38350bc4304f9bb16326f2b006e8
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-07-30 00:50:29 +0530

    var and val

--- src/02varandval.kt
+++ src/02varandval.kt
@@ -3,7 +3,7 @@ fun main(){
     var wholeNumber=10
     var fractionalNumber=0.5
     var sentence="i am kapil kumar"
-    var condition=true;
+    var condition=true
     var num : Int =20
     var num2 : Float
     num2=12.5f
@@ -22,8 +22,8 @@ fun main(){
     println(num)
     println(num2)
 
-    wholeNumber=wholeNumber+10
-    fractionalNumber=fractionalNumber+1.562
+    wholeNumber += 10
+    fractionalNumber += 1.562
     sentence="hello kapil"
     condition=false
     num+=20
