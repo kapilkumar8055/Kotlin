063b09298de9d0ab8a0c5445295ee0689804008e
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-07-30 00:46:43 +0530

    var and val

--- src/02varandval.kt
+++ src/02varandval.kt
@@ -4,6 +4,9 @@ fun main(){
     var fractionalNumber=0.5
     var sentence="i am kapil kumar"
     var condition=true;
+    var num : Int =20
+    var num2 : Float
+    num2=12.5f
 
     //val-immutable
     val number=12
@@ -11,11 +14,32 @@ fun main(){
     val sentence2="i am from mathura"
     val condition2=true
 
+    println("var variables")
     println(wholeNumber)
     println(fractionalNumber)
     println(sentence)
     println(condition)
+    println(num)
+    println(num2)
 
+    wholeNumber=wholeNumber+10
+    fractionalNumber=fractionalNumber+1.562
+    sentence="hello kapil"
+    condition=false
+    num+=20
+    num2+=12.5f
+
+    println()
+    println("var variables after changing the value")
+    println(wholeNumber)
+    println(fractionalNumber)
+    println(sentence)
+    println(condition)
+    println(num)
+    println(num2)
+
+    println()
+    println("val variables")
     println(number)
     println(fraction)
     println(sentence2)
