556149979a93886b808ae8222d045077d879f5ad
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-07-30 23:37:19 +0530

    Arithmetic operator

---
+++ src/03ArithmeticOperators.kt
@@ -0,0 +1,31 @@
+fun main(){
+    val number1=10
+    val number2=12
+    val number3=5.0
+    val number4=6.0
+    var integerNumber : Int
+    var doubleNumber :Double
+
+    //Arithmetic operators
+    integerNumber=number1+number2
+    doubleNumber=number3+number4
+    println("$number1 + $number2 = $integerNumber")
+    println("$number3 + $number4 = $doubleNumber")
+    integerNumber=number1-number2
+    doubleNumber=number3-number4
+    println("$number1 - $number2 = $integerNumber")
+    println("$number3 - $number4 = $doubleNumber")
+    integerNumber=number1*number2
+    doubleNumber=number3*number4
+    println("$number1 * $number2 = $integerNumber")
+    println("$number3 * $number4 = $doubleNumber")
+    integerNumber=number1/number2
+    doubleNumber=number3/number4
+    println("$number1 / $number2 = $integerNumber")
+    println("$number3 / $number4 = $doubleNumber")
+    integerNumber=number1%number2
+    doubleNumber=number3%number4
+    println("$number1 % $number2 = $integerNumber")
+    println("$number3 % $number4 = $doubleNumber")
+    println(10/2)
+}
