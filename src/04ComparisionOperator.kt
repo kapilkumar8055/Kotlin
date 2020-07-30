13ab1fdae6e03443830ce4ac79f5ee242ba18c85
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-07-31 00:44:10 +0530

    Comparision Operator

---
+++ src/04ComparisionOperator.kt
@@ -0,0 +1,28 @@
+/*
+ * Created by KAPIL KUMAR
+ * 31-07-2020
+ * 12:04 AM
+ **/
+fun main(){
+    val number1=10
+    val number2=5
+    var result : Boolean
+
+    result=number1==number2
+    println("$number1 == $number2 is $result")
+
+    result=number1!=number2
+    println("$number1 != $number2 is $result")
+
+    result=number1<number2
+    println("$number1 < $number2 is $result")
+
+    result=number1>number2
+    println("$number1 > $number2 is $result")
+
+    result=number1<=number2
+    println("$number1 <= $number2 is $result")
+
+    result=number1>=number2
+    println("$number1 >= $number2 is $result")
+}
