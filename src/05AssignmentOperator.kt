5eda6206cf5c5c31393097972d6c3069320d42f1
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-07-31 01:18:20 +0530

    Assignment Operator

---
+++ src/05AssignmentOperator.kt
@@ -0,0 +1,39 @@
+/*
+ * Created by KAPIL KUMAR
+ * 31-07-2020
+ * 12:48 AM
+ **/
+fun main() {
+    val number = 10
+    var result = 20
+
+    println("result = $result")
+    println("number = $number")
+    result += number
+    println("result += number = $result")
+    println("----------------------------------------")
+
+    println("result = $result")
+    println("number = $number")
+    result -= number
+    println("result -= number = $result")
+    println("----------------------------------------")
+
+    println("result = $result")
+    println("number = $number")
+    result *= number
+    println("result *= number = $result")
+    println("----------------------------------------")
+
+    println("result = $result")
+    println("number = $number")
+    result /= number
+    println("result /= number = $result")
+    println("----------------------------------------")
+
+    println("result = $result")
+    println("number = $number")
+    result %= number
+    println("result %= number = $result")
+    println("----------------------------------------")
+}
