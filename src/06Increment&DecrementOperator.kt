8d844caa535c49af9e546ad6f76823b29e0154d3
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-08-03 17:34:18 +0530

    Increment and Decrement Operators

---
+++ src/06Increment&DecrementOperator.kt
@@ -0,0 +1,19 @@
+/*
+ * Created by KAPIL KUMAR
+ * 03-08-2020
+ * 05:19 PM
+ **/
+fun main(){
+    var a=10
+    //postFix
+    a++
+    println(a)
+    a--
+    println(a)
+
+    //preFix
+    ++a
+    println(a)
+    --a
+    println(a)
+}
