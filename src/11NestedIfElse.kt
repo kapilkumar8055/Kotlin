63e72b368739c0296fdd7ed1d0e395d96cf0d32a
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-08-04 23:18:13 +0530

    Nested If Else

---
+++ src/11NestedIfElse.kt
@@ -0,0 +1,17 @@
+/*
+ * Created by KAPIL KUMAR
+ * 04-08-2020
+ * 11:06 PM
+ **/
+fun main(){
+    val a=20
+    if(a<=5){
+        println("$a is less than 5")
+    }
+    else if(a<=10){
+        println("$a is less than 10 and Greater than 5")
+    }
+    else{
+        println("$a is greater than 10")
+    }
+}
