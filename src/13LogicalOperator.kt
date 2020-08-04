9a5a1e589fedb8c49e5121276a362ebd71b21f65
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-08-04 23:34:17 +0530

    Logical Operator

---
+++ src/13LogicalOperator.kt
@@ -0,0 +1,21 @@
+/*
+ * Created by KAPIL KUMAR
+ * 04-08-2020
+ * 11:24 PM
+ **/
+fun main(){
+    /*
+    Logical Operator
+    1-Logical OR --- it is denoted by ||
+    2-Logical AND --- it is denoted by &&
+    3-Logical NOT --- it is denoted by !
+     */
+
+    val num=5
+    if(num<0 || num>0 ){
+        println("Number is non-zero")
+    }
+    else{
+        println("Number is Zero")
+    }
+}
