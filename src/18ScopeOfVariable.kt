0a99a2906dbc3e49eacf30e1322b12af28aefe81
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-08-07 22:53:30 +0530

    Scope of a variable

---
+++ src/18ScopeOfVariable.kt
@@ -0,0 +1,17 @@
+/*
+ * Created by KAPIL KUMAR
+ * 07-08-2020
+ * 10:36 PM
+ **/
+fun main(){
+    //block of code 1
+    var a=10
+    for(i in 1..5){
+        //block of code 2
+        a += 1
+        var b=20
+        println(i)
+    }
+    println(a)
+    println("here when i used variable b program gives an error")
+}
